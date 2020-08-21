a16e958cfc3c55d6b18fcf7c0c9c125ed8167f3d
Manuel Louie Gayao <louiemanuel238@gmail.com>
2020-08-20 21:26:12 -0600

    setting up state and props

--- src/Product.js
+++ src/Product.js
@@ -3,22 +3,7 @@ import React from "react";
 export default function Product({item}) {
   return (
     <div>
-<<<<<<< HEAD
-      
-      
-=======
-      <div>
-        <img src="" />
-      </div>
-      
-        <div>
-          <a>Fort Knight </a>
-          <p>Authentic renaissance actors, delivered in just two weeks.</p>
-          <p>Added paragraph</p>
-        </div>
->>>>>>> d5a513dab84a634eab539404128a0afb45953b0b
-       
-       
+
       <div className="Louies-DIV-for-Now">
           {console.log(item)}
            <div>
